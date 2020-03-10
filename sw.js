74829f7062d598248d88ec18321c02f6f770996d
Chauveau <ludovic.chauveau@enablon.com>
2020-03-10 23:19:00 +0100

    first pwa github

--- sw.js
+++ sw.js
@@ -1,43 +1,58 @@
-var staticCacheName = 'myNewsSite-v0';
+var APP_PREFIX = 'ApplicationName_'     // Identifier for this app (this needs to be consistent across every cache update)
+var VERSION = 'version_01'              // Version of the off-line cache (change this value everytime you want to update cache)
+var CACHE_NAME = APP_PREFIX + VERSION
+var URLS = [                            // Add URL you want to cache in this list.
+  '/{repository}/',                     // If you have separate JS/CSS files,
+  '/{repository}/index.html'            // add path to those files here
+]
 
-self.addEventListener('install', function (event) {
-  console.log('ServiceWorker (' + staticCacheName + '): install called');
-  event.waitUntil(
-    caches.open(staticCacheName).then(function (cache) {
-      return cache.addAll([
-        '/',
-        'index.html',
-        'manifest.json',
-        'lib/jquery.min.js',
-        'lib/bootstrap.min.css',
-        'custom.css',
-        'feedReader.js',
-      ]);
+// Respond with cached resources
+self.addEventListener('fetch', function (e) {
+  console.log('fetch request : ' + e.request.url)
+  e.respondWith(
+    caches.match(e.request).then(function (request) {
+      if (request) { // if cache is available, respond with cache
+        console.log('responding with cache : ' + e.request.url)
+        return request
+      } else {       // if there are no cache, try fetching request
+        console.log('file is not cached, fetching : ' + e.request.url)
+        return fetch(e.request)
+      }
+
+      // You can omit if/else for console.log & put one line below like this too.
+      // return request || fetch(e.request)
     })
-  );
-});
+  )
+})
 
-self.addEventListener('activate', function (event) {
-  console.log('ServiceWorker: Activate');
-  //activate active worker asap
-  event.waitUntil(self.clients.claim());
-});
-
-self.addEventListener('fetch', function (event) {
-
-  //handle live reload function (for develop purpose)
-  if (event.request.url.indexOf('/browser-sync/') !== -1) {
-    //fetch(..) is the new XMLHttpRequest
-    event.respondWith(fetch(event.request));
-    return;
-  }
-
-  console.log('ServiceWorker: fetch called for ' + event.request.url);
-
-  //if request in cache then return it, otherwise fetch it from the network
-  event.respondWith(
-    caches.match(event.request).then(function (response) {
-      return response || fetch(event.request);
+// Cache resources
+self.addEventListener('install', function (e) {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(function (cache) {
+      console.log('installing cache : ' + CACHE_NAME)
+      return cache.addAll(URLS)
     })
-  );
-});
+  )
+})
+
+// Delete outdated caches
+self.addEventListener('activate', function (e) {
+  e.waitUntil(
+    caches.keys().then(function (keyList) {
+      // `keyList` contains all cache names under your username.github.io
+      // filter out ones that has this app prefix to create white list
+      var cacheWhitelist = keyList.filter(function (key) {
+        return key.indexOf(APP_PREFIX)
+      })
+      // add current cache name to white list
+      cacheWhitelist.push(CACHE_NAME)
+
+      return Promise.all(keyList.map(function (key, i) {
+        if (cacheWhitelist.indexOf(key) === -1) {
+          console.log('deleting cache : ' + keyList[i] )
+          return caches.delete(keyList[i])
+        }
+      }))
+    })
+  )
+})
