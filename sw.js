f3f8b450f98ab7b119d5ec7987b4b618a20f3cac
Chauveau <ludovic.chauveau@enablon.com>
2020-03-11 23:13:11 +0100

    install amp

--- sw.js
+++ sw.js
@@ -1,3 +1,6 @@
+importScripts('https://cdn.ampproject.org/sw/amp-sw.js');
+AMP_SW.init();
+
 var APP_PREFIX = 'ApplicationName_'     // Identifier for this app (this needs to be consistent across every cache update)
 var VERSION = 'version_01'              // Version of the off-line cache (change this value everytime you want to update cache)
 var CACHE_NAME = APP_PREFIX + VERSION
